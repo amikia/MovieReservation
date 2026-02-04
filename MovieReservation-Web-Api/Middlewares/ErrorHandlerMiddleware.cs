5855556c26d27fd034294b0a64906194232142d8
amikia <amirkian81@gmail.com>
2026-02-05 00:03:17 +0330

    feat : add custom exception, api response and an error handling middleware

---
+++ MovieReservation-Web-Api/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using MovieReservation.Shared.Classes;
+using MovieReservation.Shared.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace MovieReservation_Web_Api.Middlewares;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            var responseModel = ApiResponse<string>.Fail(error.Message);
+            switch (error)
+            {
+                case CustomException e:
+                    // custom application error
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case KeyNotFoundException e:
+                    // NotFound error
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                case UnauthorizedAccessException e:
+                    // Unauthorized error
+                    response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    break;
+                default:
+                    // unhandled error
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+            //var result = JsonSerializer.Serialize(responseModel);
+
+            var result = JsonSerializer.Serialize(responseModel, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            await response.WriteAsync(result);
+        }
+    }
+}
